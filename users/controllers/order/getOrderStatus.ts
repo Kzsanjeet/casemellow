d48979122658d4e4fda7a6a186865e215ba78048
Kzsanjeet <sanjeetkazithapa@gmail.com>
2025-05-11 16:42:51 +0545

    updated the order tracking function

--- users/controllers/order/getOrderStatus.ts
+++ users/controllers/order/getOrderStatus.ts
@@ -1,24 +1,35 @@
 import { Request, Response } from "express";
 import Order from "../../../admin/models/order.models/order";
+import CustomizeOrder from "../../../admin/models/order.models/customizeOrder";
 
-const getOrderStatus = async(req:Request, res:Response):Promise<void> => {
-    try {
-        const {trackOrderId} = req.body;
-        if(!trackOrderId){
-            res.status(404).json({success:false, message:"Order id is required"})
-            return
-        }
-        const orderStatus = await Order.findOne({ trackOrderId }).select("orderStatus");
-        if(!orderStatus){
-            res.status(404).json({success:false, message:"Order status not found"});
-            return
-        }
-        res.status(200).json({success:true, message:"Order status fetched", data:orderStatus})
-    } catch (error) {
-        if(error instanceof(Error)){
-            res.status(400).json({success:false, message:error.message})
-        }
+const getOrderStatus = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { trackOrderId } = req.body;
+
+    if (!trackOrderId) {
+      res.status(400).json({ success: false, message: "Order ID is required" });
+      return;
     }
-}
+
+    const orderStatus = await Order.findOne({ trackOrderId }).select("orderStatus");
+
+    if (orderStatus) {
+      res.status(200).json({ success: true, message: "Order status fetched", data: orderStatus });
+      return;
+    }
+
+    const customOrderStatus = await CustomizeOrder.findOne({ trackOrderId }).select("orderStatus");
+
+    if (customOrderStatus) {
+      res.status(200).json({ success: true, message: "Order status fetched", data: customOrderStatus });
+    } else {
+      res.status(404).json({ success: false, message: "Order not found" });
+      return
+    }
+  } catch (error) {
+    console.error("Error fetching order status:", error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+};
 
 export default getOrderStatus;
